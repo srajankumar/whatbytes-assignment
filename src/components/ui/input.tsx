94abad630e5be12623ea74f9f1e4d72cba964b59
srajan <kumarsrajan02@gmail.com>
2024-10-05 14:00:42 +0530

    tslint

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface InputProps
-  // tslint:disable-next-line:no-empty-interface
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
