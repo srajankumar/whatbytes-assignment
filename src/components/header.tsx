1a8b59ebed343992e3d7726877337241893b7635
srajan <kumarsrajan02@gmail.com>
2024-10-05 14:10:19 +0530

    minor ui changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,7 @@ import Image from "next/image";
 const Header = () => {
   return (
     <nav className="h-24 w-full border-b flex items-center justify-between px-5">
-      <Link
-        href={"/"}
-        className="flex md:text-3xl text-2xl font-bold gap-2 items-center"
-      >
+      <Link href={"/"} className="flex gap-2 items-center">
         <Image
           src={"/logo.svg"}
           alt="WhatBytes"
@@ -16,14 +13,14 @@ const Header = () => {
           height={500}
           className="md:w-12 w-8 md:h-12 h-8"
         />
-        <p>WhatBytes</p>
+        <p className="md:text-3xl text-xl font-bold">WhatBytes</p>
       </Link>
-      <div className="flex text-lg font-bold gap-3 items-center border rounded-md shadow-sm px-3 py-2">
+      <div className="flex gap-3 items-center border rounded-md shadow-sm px-3 py-2">
         <Avatar className="w-7 h-7">
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>SK</AvatarFallback>
         </Avatar>
-        <h1>Srajan Kumar</h1>
+        <h1 className="font-bold md:text-lg text-base">Srajan Kumar</h1>
       </div>
     </nav>
   );
